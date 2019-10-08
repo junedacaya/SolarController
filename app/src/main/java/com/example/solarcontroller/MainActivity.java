2f692a5e9c2be7bd95b5b74d153e16e91816cd29
junedacaya <54863402+junedacaya@users.noreply.github.com>
2019-10-08 11:36:36 -0400

    change nav text and color

--- app/src/main/java/com/example/solarcontroller/MainActivity.java
+++ app/src/main/java/com/example/solarcontroller/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.solarcontroller;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
